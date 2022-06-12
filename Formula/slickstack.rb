00161285e3579b93eb962c5ae298615a8ee1b5c0
Jesse Nickles <jesse@littlebizzy.com>
2022-06-12 19:20:43 +0700

    Update slickstack.rb

--- Formula/slickstack.rb
+++ Formula/slickstack.rb
@@ -4,15 +4,18 @@ class SlickStack < Formula
   url "https://raw.githubusercontent.com/littlebizzy/slickstack/master/bash/ss-install.txt, :using => :wget"
   mirror ="https://gitlab.com/littlebizzy/slickstack/-/raw/master/bash/ss-install.txt, :using => :wget"
 
+  depends_on "bash"
   depends_on "nginx"
   depends_on "php"
-  depends_on :mysql
+  depends_on :mysql # remote okay
 
   def install
-    bin.install "easyengine.phar" => "ee"
+    bin.install "ss-install.txt" => "ss"
   end
 end
 
 # https://github.com/syhw/homebrew/blob/master/Library/Contributions/example-formula.rb
 # https://cfxdesign.com/lemp-stack-for-mackbook-pro-m1-2022/
 # https://kevdees.com/macos-11-big-sur-nginx-setup-multiple-php-versions/
+# https://willcarh.art/blog/a-foray-into-homebrew
+# https://stackoverflow.com/questions/26392445/rename-files-in-install-part-of-homebrew-formula
